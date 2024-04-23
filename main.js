70aa8075d7555e3a31d2162e11a9f98f78872c22
souxas <107128939+souxas@users.noreply.github.com>
2024-04-23 15:36:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
 
@@ -34,6 +42,14 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+
+    for (let i=0; i<contadores.length;i++){
+       // contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
 }
 
 function comecaCronometro(){
